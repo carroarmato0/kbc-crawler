57edc0def8e18e3598296b2c86c6d8475fbb919a
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2013-10-07 22:31:59 +0200

    Fix output when no config.ini file is found
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- src/Crawler.php
+++ src/Crawler.php
@@ -7,7 +7,7 @@ class Crawler {
   }
 
   private function loadconf() {
-    $ini_array = @parse_ini_file('config.ini', true) or die("Couldn't find a conf.ini file");
+    $ini_array = @parse_ini_file('config.ini', true) or die("Couldn't find a config.ini file\n");
     return $ini_array;
   }
 
